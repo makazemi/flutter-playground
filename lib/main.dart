86413feec5c93fe78b5ff29805554b788caf8a85
mary8 <mary8shtr@gmail.com>
2021-03-06 12:14:06 +0330

    write transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:sample_flutter_web_app/result.dart';
-import 'quiz.dart';
-import 'result.dart';
+import 'package:sample_flutter_web_app/new_transaction.dart';
+import 'transaction.dart';
+import 'transaction_list.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyHomePage());
 
-class MyApp extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _questions = [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 3},
-        {'text': 'Snake', 'score': 11},
-        {'text': 'Elephant', 'score': 5},
-        {'text': 'Lion', 'score': 9},
-      ],
-    },
-    {
-      'questionText': 'Who\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-      ],
-    },
-  ];
-  var _totalScore = 0;
+class _MyHomePageState extends State<MyHomePage> {
 
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      if (_questionIndex < _questions.length) _questionIndex += 1;
-    });
-    print("answer chosen");
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
   }
 
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
+
+  void _startAddNewTransaction(BuildContext ctx){
+    showModalBottomSheet(context: ctx, builder: (_){
+      return NewTransaction(_addNewTransaction);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: ()=> _startAddNewTransaction(context),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              TransactionList(_userTransactions),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: ()=> _startAddNewTransaction(context),
+        ),
       ),
-      body: _questionIndex < _questions.length
-          ? Quiz(
-              answerQuestion: _answerQuestion,
-              questionIndex: _questionIndex,
-              questions: _questions,
-            )
-          : Result(_totalScore,_resetQuiz),
-    ));
+    );
   }
 }
+
+
